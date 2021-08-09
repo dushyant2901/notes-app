3152ccd16e1767a6fcbd8ae98d58f727edad24e3
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-08-09 19:28:49 +0530

    added till updateUI functionality

---
+++ app.js
@@ -0,0 +1,104 @@
+const addBtn = document.getElementById("add-notes");
+const notesTitle = document.getElementById("notes-title");
+const notesText = document.getElementById("notes-text");
+const notesContainer = document.querySelector(".notes-container");
+const myNoteDiv = document.querySelector(".note");
+
+addBtn.addEventListener("click", addNote);
+
+function addNote(e) {
+    e.preventDefault();
+    validateInput();
+
+    clearInputs([notesTitle, notesText]);
+}
+
+function validateInput() {
+    if (!notesText.value || !notesTitle.value) {
+        alert("Notes Title and Text can not be blank");
+    } else {
+        noteDiv(notesTitle, notesText);
+        getNotes();
+        setNotes();
+    }
+}
+
+/*function noteDiv(title, text) {
+    let str = "";
+    
+    let note = ` <div class="note">
+    <p class="note-counter">Note-1</p>
+    <h3 class="note-heading">${title.value}</h3>
+    <p class="note-text">${text.value}</p>
+    <button class="edit">Edit</button>
+    <button class="delete">Delete</button>
+</div>`;
+    str = str + note;
+    notesContainer.innerHTML = str;
+}*/
+function noteDiv() {
+    getNotes();
+    let str = "";
+    myNotes.forEach((note, index) => {
+        let noteu = ` <div class="note">
+    <p class="note-counter">Note:${index + 1}</p>
+    <h3 class="note-heading">${note.title}</h3>
+    <p class="note-text">${note.text}</p>
+    <button class="edit">Edit</button>
+    <button class="delete">Delete</button>
+    </div>`;
+        str = str + noteu;
+    })
+    notesContainer.innerHTML = str
+}
+
+function clearInputs(inputs) {
+    inputs.forEach((input) => {
+        input.value = "";
+    });
+}
+
+function getNotes() {
+    let notes = localStorage.getItem("notes");
+
+    checkNoteStorage(notes);
+}
+
+function checkNoteStorage(notes) {
+
+    if (notes == null) {
+        myNotes = [];
+    } else {
+        myNotes = JSON.parse(notes);
+    }
+}
+
+function editOrDelete(event) { 
+    let toEditOrDel=event.target
+    if()
+}
+
+function editNote() { }
+
+function deleteNote() { }
+
+function updateUi() {
+    //getNotes()
+    noteDiv()
+
+
+
+}
+updateUi()
+
+function setNotes() {
+    getNotes();
+    // checkNoteStorage() nno need of this as we are already passing it in getnotes
+    let myNoteObj = {
+        title: notesTitle.value,
+        text: notesText.value,
+    };
+    myNotes.push(myNoteObj);
+
+    localStorage.setItem("notes", JSON.stringify(myNotes));
+}
